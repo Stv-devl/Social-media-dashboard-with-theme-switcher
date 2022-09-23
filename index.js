edc393b38e4bd26e9d787ebdcfa7b2580563bb39
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-09-23 17:21:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
+const switchs = document.querySelector(".switchs");
 const light = document.querySelector(".light");
 const dark = document.querySelector(".dark");
 const body = document.querySelector("body");
 
 body.classList.add("themedark");
 
-light.addEventListener("click", (e) => {
-  body.classList.add("themelight");
-  body.classList.remove("themedark");
-  light.style.opacity = "1";
-  dark.style.opacity = "0";
-});
-dark.addEventListener("click", (e) => {
-  body.classList.add("themedark");
-  body.classList.remove("themelight");
-  light.style.opacity = "0";
-  dark.style.opacity = "1";
+switchs.addEventListener("click", () => {
+  document.body.classList.toggle("themelight");
+  dark.classList.toggle("darkclicked");
 });
