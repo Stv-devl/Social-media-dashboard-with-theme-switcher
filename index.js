c08d3260f1e688efca0dd4269dc28371f676daa2
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-09-25 13:31:19 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const switchs = document.querySelector(".switchs");
 const dark = document.querySelector(".dark");
 const body = document.querySelector("body");
 
-//add dark theme like default
-body.classList.add("themedark");
-
 //toggle between light and dark theme, add darkclicked for move the switch button
 switchs.addEventListener("click", () => {
   body.classList.toggle("themelight");
